f82b94ec78ea7a60452a846c7962184c62c194be
tetuns <sderosa23@fed.idserve.net>
2023-11-15 11:21:35 +0100

    Initial Commit

---
+++ clock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  clock
+//
+//  Created by Stefano De Rosa on 15/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ clock/clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockApp.swift
+//  clock
+//
+//  Created by Stefano De Rosa on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
